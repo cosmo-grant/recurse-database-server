43ceb226dc87ffc06bb06f179b305c962fad5227
Cosmo Grant <cosmo.grant@postcodelottery.co.uk>
2026-06-13 19:23:13 +0100

    minimal echo server

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import socket
+
+
 def main():
-    print("Hello from recurse-database-server!")
+    server = socket.create_server(("localhost", 4000))
+    conn, _ = server.accept()
+    request = b""
+    while data := conn.recv(128):
+        request += data
+        # I assume all requests are body-free, so end of message is end of headers.
+        if request.endswith(b"\r\n\r\n"):
+            break
+    print(request)
 
 
 if __name__ == "__main__":
