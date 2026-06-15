b6338588553116e8c6cffbb9db50d07e462546bb
Cosmo Grant <cosmo.grant@postcodelottery.co.uk>
2026-06-15 19:21:24 +0100

    assume POST instead of GET for sets

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ from main import Request, Server, handle_request, parse
 
 
 def test_parse_set_request():
-    assert parse(b"GET /set?somekey=somevalue HTTP/1.1\r\n\r\n") == Request("somekey", "somevalue")
+    assert parse(b"POST /set?somekey=somevalue HTTP/1.1\r\n\r\n") == Request("somekey", "somevalue")
 
 
 def test_parse_get_request():
@@ -30,7 +30,7 @@ def test_e2e_get_then_set():
     server = Server()
     thread = Thread(target=server.start)
     thread.start()
-    response = requests.get("http://localhost:4000/set", params={"somekey": "somevalue"})
+    response = requests.post("http://localhost:4000/set", params={"somekey": "somevalue"})
     assert response.status_code == 200
     response = requests.get("http://localhost:4000/get", params={"key": "somekey"})
     assert response.text == "somevalue"
